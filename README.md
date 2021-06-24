c891c7d3c162c47b755c6154d0bc5eb5123eb10d
Ali Burzhi <aliburzhi033@gmail.com>
2021-06-24 20:22:36 +0300

    Отправка данных на сервер
    Добавил отправку данных из форм на сервер

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # practice2
+
+Food Project for everyone!
--- js/script.js
+++ js/script.js
@@ -225,14 +225,67 @@ class MenuCard {
         '.menu .container'
     ).render();
     
+// END OF ИСПОЛЬЗУЕМ КЛАССЫ ДЛЯ КАРТОЧЕК --------------------------------
 
+    // ОТПРАЛЯЕМ ДАННЫЕ ИЗ ФОРМ НА СЕРВЕР
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с Вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');  
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
     
 
 
 
 
-// END OF ИСПОЛЬЗУЕМ КЛАССЫ ДЛЯ КАРТОЧЕК --------------------------------
-
+    // ОТПРАЛЯЕМ ДАННЫЕ ИЗ ФОРМ НА СЕРВЕР
 
     // END OF LOADED  
 });
