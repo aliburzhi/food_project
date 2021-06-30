f29101ff17bd26bedd1ffb0948dc1d2719c9d6f6
Ali Burzhi <aliburzhi033@gmail.com>
2021-06-30 13:10:11 +0300

    add Slider on Project

--- js/script.js
+++ js/script.js
@@ -320,10 +320,65 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // ДЕЛАЕМ СЛАЙДЕР В ПРОЕКТЕ
 
-    
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
 
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+    }
+
+        
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 
     // КОНЕЦ СЛАЙДЕРА В ПРОЕКТЕ
+
+
+
+
+
+
+
 
     // END OF LOADED  
 });
